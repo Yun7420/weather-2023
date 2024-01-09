8a3bc0e75411a81caeddb8b21747345427901d33
Sangyun Han <hsy742010@gmail.com>
2024-01-09 16:57:46 +0900

    weather project update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
 // SCSS 스타일
-import './assets/scss/style.scss';
+import "./assets/scss/style.scss";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  <React.StrictMode>
     <App />
+  </React.StrictMode>
 );
